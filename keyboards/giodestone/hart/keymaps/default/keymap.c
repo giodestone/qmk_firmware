a2a08e76918e32393c0de7a54bc014425024e4dd
Feliks <118727467+FeliksJakimowKonglomerate@users.noreply.github.com>
2025-03-22 00:34:21 +0000

    Fixed autoformatting breaking my code!

--- keyboards/giodestone/hart/keymaps/default/keymap.c
+++ keyboards/giodestone/hart/keymaps/default/keymap.c
@@ -65,26 +65,26 @@ enum layer_names {
     |       2 | LShift | \  | Z   | X   | C     | V     | B     | N     | M     | ,     | .    | /     | RShift | UP     | End   |
     |       1 | LCtrl  | Fn | Win | Alt | !NULL | !Mac3 | Space | !Mac2 | !Mac1 | !PotB | RAlt | RCtrl | LEFT   | DOWN   | RIGHT |
     +---------+--------+----+-----+-----+-------+-------+-------+-------+-------+-------+------+-------+--------+--------+-------+
-        KC_ESC,        KC_F1,               KC_F2,       KC_F3,       KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,               KC_F10,       KC_F11,        KC_F12,         KC_PRINT_SCREEN, KC_DEL,
     */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Base Layer
     [_QWERTY] = LAYOUT(
+        KC_ESC,        KC_F1,               KC_F2,       KC_F3,       KC_F4,   KC_F5,    KC_F6,    KC_F7,   KC_F8,    KC_F9,               KC_F10,       KC_F11,        KC_F12,         KC_PRINT_SCREEN, KC_DEL,
         KC_GRV,        KC_1,                KC_2,        KC_3,        KC_4,    KC_5,     KC_6,     KC_7,    KC_8,     KC_9,                KC_0,         KC_MINS,       KC_EQL,         KC_BSPC,         KC_HOME,
         KC_TAB,        KC_Q,                KC_W,        KC_E,        KC_R,    KC_T,     KC_Y,     KC_U,    KC_I,     KC_O,                KC_P,         KC_LBRC,       KC_RBRC,        KC_INSERT,       KC_PGUP,
         KC_CAPS,       KC_A,                KC_S,        KC_D,        KC_F,    KC_G,     KC_H,     KC_J,    KC_K,     KC_L,                KC_SEMICOLON, KC_QUOTE,      KC_NONUS_HASH,  KC_ENTER,        KC_PGDN,
         KC_LEFT_SHIFT, KC_NONUS_BACKSLASH,  KC_Z,        KC_X,        KC_C,    KC_V,     KC_B,     KC_N,    KC_M,     KC_COMMA,            KC_DOT,       KC_SLASH,      KC_RIGHT_SHIFT, KC_UP,           KC_END,
         KC_LEFT_CTRL,  MO(_FN1),            KC_LEFT_GUI, KC_LEFT_ALT, XXXXXXX, KC_F15,   KC_SPACE, KC_F14,  KC_F13,  KC_MEDIA_PLAY_PAUSE,  KC_RIGHT_ALT, KC_RIGHT_CTRL, KC_LEFT,        KC_DOWN,         KC_RIGHT
-        _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY,          KC_MNXT, _______, _______, _______, _______, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, _______, _______,
     ),
 
     // Fn Layer
     [_FN1] = LAYOUT(
+        _______, KC_MUTE, KC_VOLD, KC_VOLU, KC_MPRV, KC_MPLY,          KC_MNXT, _______, _______, _______, _______, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, _______, _______,
         _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,            _______,          _______, _______,
         _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,            _______,          _______, _______,
         _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,            _______,          _______, _______,
         _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,            _______,          _______, _______,
-        _______, _______, _______, _______, _______, QK_BOOTLOADER,    _______, _______, _______, _______, _______,            _______,          _______, _______, _______
+        _______, _______, _______, _______, _______, QK_BOOTLOADER,    _______, _______, _______, _______, _______, _______,            _______,          _______, _______
     )
 };
 
